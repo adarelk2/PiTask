2b74291ee38de81dd361889ac76ad7daeede7595
adarelk2 <78350796+Adar-Istoreil@users.noreply.github.com>
2025-07-07 22:47:39 +0300

    domain validation

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,8 @@ const Application = require('../core/Application');
 
 // Handles '/' and '/:controller'
 router.get('/:controller?', (req, res) => {
-  console.log("test");
-  // res.send("gfdgfd");
-  res.render("test", {title:"test"});
-
-  // const app = new Application(req, res);
-  // app.init();
+  const app = new Application(req, res);
+  app.init();
 });
 
 module.exports = router;
