a45d989a5dbc1600b2323e7aaa0808e7d381edf4
adarelk2 <78350796+Adar-Istoreil@users.noreply.github.com>
2025-07-07 22:43:39 +0300

    test

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,12 @@ const Application = require('../core/Application');
 
 // Handles '/' and '/:controller'
 router.get('/:controller?', (req, res) => {
-  const app = new Application(req, res);
-  app.init();
+  console.log("test");
+  // res.send("gfdgfd");
+  res.render("test", {title:"test"});
+
+  // const app = new Application(req, res);
+  // app.init();
 });
 
 module.exports = router;
