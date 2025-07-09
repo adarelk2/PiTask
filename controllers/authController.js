862539d1355ff2ea6877f04eb03015e461a677f3
adarelk2 <78350796+Adar-Istoreil@users.noreply.github.com>
2025-07-09 22:58:59 +0300

    test

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const UserModel = require('../models/UserModel');
+const jwt = require('jsonwebtoken');
 const userModel = new UserModel();
 require('dotenv').config();
 
