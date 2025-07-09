dc5bad4e60c1992bc99ec4f2ce9b0e5517590066
adarelk2 <78350796+Adar-Istoreil@users.noreply.github.com>
2025-07-10 00:24:53 +0300

    test

--- public/js/init.js
+++ public/js/init.js
@@ -1,11 +1,7 @@
 // public/js/init.js
 (function () {
-    const token = localStorage.getItem('token');
-    if (token) {
-      window.__AUTH_TOKEN__ = token;
-    }
-  
-    // טען את המשתמש מהשרת
+  const token = localStorage.getItem('token');
+  if (token) {
     fetch('/auth/getUserByToken', {
       headers: {
         Authorization: 'Bearer ' + token
@@ -13,14 +9,12 @@
     })
     .then(res => res.ok ? res.json() : Promise.reject('unauthenticated'))
     .then(data => {
-      // שמור את המשתמש כ־window.user
       window.user = data.user;
-      // תוכל למשל להציג אותו:
-      const userSpan = document.getElementById('user-info');
-      if (userSpan) userSpan.innerText = `Welcome ${user.username}`;
+      document.getElementById('user-info').innerText = `Welcome ${user.username}`;
     })
     .catch(err => {
       console.warn('User not logged in:', err);
     });
+  }
   })();
   
