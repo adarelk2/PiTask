d6cc7060ffb1d6d1d5b9339e210156cb6081803b
adarelk2 <78350796+Adar-Istoreil@users.noreply.github.com>
2025-07-09 23:03:31 +0300

    test

--- controllers/authController.js
+++ controllers/authController.js
@@ -70,7 +70,7 @@ exports.verifyToken = async (req, res) => {
       maxAge: 1000 * 60 * 60 * 24 * 7
     });
 
-    res.json({ success: true, user: req.session.user });
+    res.redirect('/');
   } catch (err) {
     console.error('Token verification failed or DB error:', err);
     res.status(401).json({ success: false, message: 'Invalid token or DB error' });
