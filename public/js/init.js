f23ab425fefcfe86e2365970c706ca74a36166fc
adarelk2 <78350796+Adar-Istoreil@users.noreply.github.com>
2025-07-10 18:39:00 +0300

    test

--- public/js/init.js
+++ public/js/init.js
@@ -1,9 +1,12 @@
 
   document.addEventListener('DOMContentLoaded', function () {
     const token = localStorage.getItem('token');
+    console.log("here 4");
+    console.log(token);
     if (!token) return;
 
     document.querySelectorAll('a[data-link]').forEach(link => {
+        console.log(link);
       const baseHref = link.getAttribute('data-link');
       link.setAttribute('href', `${baseHref}?token=${encodeURIComponent(token)}`);
     });
