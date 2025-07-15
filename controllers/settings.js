1293764e754882ba9dca6bfb65b7006f2a56889b
adarelk2 <78350796+Adar-Istoreil@users.noreply.github.com>
2025-07-15 20:29:09 +0300

    settings page

---
+++ controllers/settings.js
@@ -0,0 +1,42 @@
+const BaseController = require('../core/BaseController');
+const UserModel = require("../models/UserModel");
+const userService = require('../services/userService');
+const ERROR_MESSAGES = require('../constants/errors');
+
+class Settings extends BaseController {
+  constructor(req, res) {
+    super(req, res);
+    this.userModel = new UserModel();
+  }
+
+  async print() {
+    const user = await userService.getUserById(this.req.user.id);
+    this.render('settings', {
+      title: 'TaskPi - Settings',
+      user: user,
+      headerTitle: "TaskPi"
+    });
+  }
+
+  async update_details(_params)
+  {
+    console.log(_params);
+    const email = _params.email
+    const pi_wallet_address = _params.wallet_address;
+    if (!userService.isValidPiWallet(pi_wallet_address)) {
+      return this.json({flag:false, errors:[ERROR_MESSAGES.VALIDATION.INVALID_WALLET]});
+    }
+    const isValidEmail = typeof _params.email === "string" &&
+      /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(_params.email);
+
+    if (!isValidEmail) {
+      return this.json({flag:false, errors:[ERROR_MESSAGES.VALIDATION.INVALID_EMAIL]});
+    }
+
+    const updated = await this.userModel.update({id:this.req.user.id}, {email, pi_wallet_address})
+
+    return this.json({flag:(updated)?true:false, errors:[]});
+  }
+}
+
+module.exports = Settings;
